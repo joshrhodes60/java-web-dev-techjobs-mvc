336bbac16ee011904fc32177fa3b63bc5cf6b865
“josh” <joshrhodes60@icloud.com>
2021-06-27 14:34:16 -0500

    complete

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -28,7 +28,7 @@ public class SearchController {
    @PostMapping("results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         ArrayList<Job> jobs;
-        if (searchType.toLowerCase().equals("all") || searchType.toLowerCase().equals("")){
+        if (searchTerm.toLowerCase().equals("all") || searchTerm.toLowerCase().equals("")){
             jobs = JobData.findAll();
             model.addAttribute("title", "All Jobs");
         } else {
@@ -40,6 +40,7 @@ public class SearchController {
        model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
         model.addAttribute("jobs", jobs);
        model.addAttribute("columns", columnChoices);
+
 
         return "search";
     }
